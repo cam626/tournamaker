cb8b2c52ea6671b70073d3773738ca65381dd59c
Noah Prisament <nprisament@gmail.com>
2019-10-02 00:53:06 -0400

    Refactor parts of the web_app to allow it to be deployed to google cloud

---
+++ web_app/index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+// const csp = require(`helmet-csp`)
+// app.use(csp({
+// 	directives: {
+//     	defaultSrc: ["'self'"]  
+// 	}
+// }));
+
+app.use(function (req, res, next){
+  if (req.headers['x-forwarded-proto'] === 'https') {
+    res.redirect('http://' + req.hostname + req.url);
+  } else {
+    next();
+  }
+});
+
+app.use(express.static(__dirname));
+
+app.listen(PORT, function () {
+  console.log('Express server is up on port ' + PORT);
+});
