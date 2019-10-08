d9a5851382cc4216ee8d6a73d4c0664f593c3350
Noah Theodore Prisament <nprisament@gmail.com>
2019-10-08 16:39:14 -0400

    start working on auth

--- web_app/main/app.jsx
+++ web_app/main/app.jsx
@@ -5,8 +5,7 @@ import Header from './components/Header';
 import Landing from './Landing';
 import _404 from './_404';
 import SignIn from './components/Auth/SignIn';
-// import OnBoard from './components/Auth/OnBoard';
-// <Route path='/onboard' component={OnBoard} />
+import OnBoard from './components/Auth/OnBoard';
 
 ReactDOM.render(
 	<BrowserRouter>
@@ -14,6 +13,7 @@ ReactDOM.render(
   		<Switch>
 			<Route exact path='/' component={Landing} />
 			<Route path='/signin' component={SignIn} />
+			<Route path='/onboard' component={OnBoard} />
 			<Route path='*' component={_404} />
 		</Switch>
 	</BrowserRouter>,
